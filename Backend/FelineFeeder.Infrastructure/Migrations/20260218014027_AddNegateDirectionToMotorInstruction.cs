43308838ecad40ef7886a207a6adff3ecae81774
Dominik Schraml <dominik.schraml97@gmail.com>
2026-02-18 02:47:40 +0100

    add feed now endpoint

---
+++ Backend/FelineFeeder.Infrastructure/Migrations/20260218014027_AddNegateDirectionToMotorInstruction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FelineFeeder.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNegateDirectionToMotorInstruction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MotorInstructions_NegateDirection",
+                table: "FeedingTimes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MotorInstructions_NegateDirection",
+                table: "FeedingTimes");
+        }
+    }
+}
